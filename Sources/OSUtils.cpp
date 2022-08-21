eded9deaacaf1fde17fee51e618dc89e58eaa97a
Youda008 <youda008@gmail.com>
2022-08-21 10:42:19 +0200

    removed unused code
    ... it will stay in git history if needed

--- Sources/OSUtils.cpp
+++ Sources/OSUtils.cpp
@@ -15,38 +15,8 @@
 
 #include <QDebug>
 
-#ifdef _WIN32
-//#include <windows.h>
-#else
-//#include <X11/Xlib.h>
-//#include <X11/extensions/Xrandr.h>
-//#include <SDL2/SDL.h>
-//#include <SDL2/SDL_vulkan.h>
-#endif // _WIN32
-
 
 //======================================================================================================================
-
-#ifdef _WIN32
-//static BOOL CALLBACK enumMonitorCallback( HMONITOR hMonitor, HDC /*hdc*/, LPRECT /*lprcClip*/, LPARAM userData )
-/*{
-	QVector< MonitorInfo > * monitors = (QVector< MonitorInfo > *)userData;
-
-	MONITORINFOEXA theirInfo;
-	theirInfo.cbSize = sizeof( theirInfo );
-	if (GetMonitorInfoA( hMonitor, &theirInfo ))
-	{
-		MonitorInfo myInfo;
-		myInfo.name = theirInfo.szDevice;
-		myInfo.width = theirInfo.rcMonitor.right - theirInfo.rcMonitor.left;
-		myInfo.height = theirInfo.rcMonitor.bottom - theirInfo.rcMonitor.top;
-		myInfo.isPrimary = theirInfo.dwFlags & MONITORINFOF_PRIMARY;
-		monitors->append( myInfo );
-	}
-
-	return TRUE;
-};*/
-#endif // _WIN32
 
 QVector< MonitorInfo > listMonitors()
 {
@@ -63,86 +33,6 @@ QVector< MonitorInfo > listMonitors()
 		myInfo.isPrimary = monitorIdx == 0;
 		monitors.push_back( myInfo );
 	}
-
-#ifdef _WIN32
-
-/* WinAPI
-	EnumDisplayMonitors( nullptr, nullptr, (MONITORENUMPROC)enumMonitorCallback, (LONG_PTR)&monitors );
-*/
-
-#else
-
-/* libXrandr
-	Display * display = XOpenDisplay( nullptr );
-	if (!display)
-	{
-		qWarning() << "cannot open X Display";
-		return monitors;
-	}
-
-	int dummy1, dummy2;
-	int major, minor;
-	if (!XRRQueryExtension( display, &dummy1, &dummy2 )
-	 || !XRRQueryVersion( display, &major, &minor ))
-	{
-		qWarning() << "failed to retrieve XRandR version";
-		XCloseDisplay( display );
-		return monitors;
-	}
-
-	if (major <= 1 && minor < 5)
-	{
-		qWarning() << "RandR version at least 1.5 is required, available is only " << major << '.' << minor;
-		XCloseDisplay( display );
-		return monitors;
-	}
-
-	Window rootWindow = RootWindow( display, DefaultScreen( display ) );
-
-	Bool onlyActive = false;
-	int monitorCnt;
-	XRRMonitorInfo * xMonitors = XRRGetMonitors( display, rootWindow, onlyActive, &monitorCnt );
-	if (!xMonitors)
-	{
-		qDebug() << "failed to retrieve monitor info";
-		XCloseDisplay( display );
-		return monitors;
-	}
-
-	for (int monitorIdx = 0; monitorIdx < monitorCnt; monitorIdx++)
-	{
-		MonitorInfo myInfo;
-		myInfo.name = XGetAtomName( display, xMonitors[ monitorIdx ].name );
-		myInfo.width = xMonitors[ monitorIdx ].width;
-		myInfo.height = xMonitors[ monitorIdx ].height;
-		myInfo.isPrimary = xMonitors[ monitorIdx ].primary != 0;
-		monitors.push_back( myInfo );
-	}
-
-	XFree( xMonitors );
-	XCloseDisplay( display );
-*/
-
-/* SDL2
-	SDL_Init( SDL_INIT_VIDEO );
-
-	int monitorCnt = SDL_GetNumVideoDisplays();
-	for (int monitorIdx = 0; monitorIdx < monitorCnt; monitorIdx++)
-	{
-		MonitorInfo myInfo;
-		SDL_DisplayMode mode;
-		SDL_GetCurrentDisplayMode( monitorIdx, &mode );
-		myInfo.name = SDL_GetDisplayName( monitorIdx );
-		myInfo.width = mode.w;
-		myInfo.height = mode.h;
-		myInfo.isPrimary = monitorIdx == 0;
-		monitors.push_back( myInfo );
-	}
-
-	SDL_Quit();
-*/
-
-#endif // _WIN32
 
 	return monitors;
 }

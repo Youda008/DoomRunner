40fd167b7b7f7c319cfae1193ab6d91f363858a7
Youda008 <youda008@gmail.com>
2023-06-29 10:34:44 +0200

    fixed non-deterministic static initialization

--- Sources/Dialogs/OwnFileDialog.cpp
+++ Sources/Dialogs/OwnFileDialog.cpp
@@ -13,32 +13,37 @@
 //======================================================================================================================
 //  static file dialog wrappers
 
-static const QFileDialog::Options DontUseNativeDialogOnLinux
-	= (isWindows() || getLinuxDesktopEnv() == "KDE") ? QFileDialog::Options() : QFileDialog::Option::DontUseNativeDialog;
+static const QFileDialog::Options disableNativeDialogOnLinux( QFileDialog::Options options )
+{
+	// initialize once on first call and then re-use.
+	static const QFileDialog::Options DontUseNativeDialogOnLinux =
+		(isWindows() || getLinuxDesktopEnv() == "KDE") ? QFileDialog::Options() : QFileDialog::Option::DontUseNativeDialog;
+	return options | DontUseNativeDialogOnLinux;
+}
 
 QString OwnFileDialog::getOpenFileName(
 	QWidget * parent, const QString & caption, const QString & dir,
 	const QString & filter, QString * selectedFilter, Options options
 ){
-	return QFileDialog::getOpenFileName( parent, caption, dir, filter, selectedFilter, options | DontUseNativeDialogOnLinux );
+	return QFileDialog::getOpenFileName( parent, caption, dir, filter, selectedFilter, disableNativeDialogOnLinux( options ) );
 }
 
 QStringList OwnFileDialog::getOpenFileNames(
 	QWidget * parent, const QString & caption, const QString & dir,
 	const QString & filter, QString * selectedFilter, Options options
 ){
-	return QFileDialog::getOpenFileNames( parent, caption, dir, filter, selectedFilter, options | DontUseNativeDialogOnLinux );
+	return QFileDialog::getOpenFileNames( parent, caption, dir, filter, selectedFilter, disableNativeDialogOnLinux( options ) );
 }
 
 QString OwnFileDialog::getSaveFileName(
 	QWidget * parent, const QString & caption, const QString & dir,
 	const QString & filter, QString * selectedFilter, Options options
 ){
-	return QFileDialog::getSaveFileName( parent, caption, dir, filter, selectedFilter, options | DontUseNativeDialogOnLinux );
+	return QFileDialog::getSaveFileName( parent, caption, dir, filter, selectedFilter, disableNativeDialogOnLinux( options ) );
 }
 
 QString OwnFileDialog::getExistingDirectory(
 	QWidget * parent, const QString & caption, const QString & dir, Options options
 ){
-	return QFileDialog::getExistingDirectory( parent, caption, dir, options | DontUseNativeDialogOnLinux );
+	return QFileDialog::getExistingDirectory( parent, caption, dir, disableNativeDialogOnLinux( options ) );
 }
